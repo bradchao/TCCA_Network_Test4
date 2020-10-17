cf51d6e1b29190a95ba8adef219fa8eaeb3b899d
bradchao <brad@brad.tw>
2020-10-17 13:24:41 +0800

    15

--- app/src/main/java/tw/org/tcca/apps/test4/MainApp.java
+++ app/src/main/java/tw/org/tcca/apps/test4/MainApp.java
@@ -1,6 +1,7 @@
 package tw.org.tcca.apps.test4;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
 
@@ -9,10 +10,15 @@ import com.android.volley.toolbox.Volley;
 
 public class MainApp extends Application {
     public RequestQueue queue;
+    public SharedPreferences sp;
+    public SharedPreferences.Editor editor;
 
     @Override
     public void onCreate() {
         super.onCreate();
         queue = Volley.newRequestQueue(this);
+
+        sp = getSharedPreferences("config", MODE_PRIVATE);
+        editor = sp.edit();
     }
 }
