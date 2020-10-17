0d2246ce546c8c12eca758467f4010edc6078640
bradchao <brad@brad.tw>
2020-10-17 10:35:47 +0800

    6

--- app/src/main/res/raw/brad02.php
+++ app/src/main/res/raw/brad02.php
@@ -1,3 +1,15 @@
 <?php
-echo rand(1,49);
+
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == 'POST'){
+	$account = $_POST['account'];
+	$passwd = $_POST['passwd'];
+	echo "POST: {$account}:{$passwd}";
+}else{
+	$account = $_GET['account'];
+	$passwd = $_GET['passwd'];
+	echo "GET: {$account}:{$passwd}";
+}
+
+
 ?>
