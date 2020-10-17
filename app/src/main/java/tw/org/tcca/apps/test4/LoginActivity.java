91ed3fe06d0fc1a972e7a0eadaa538db0a6d96be
bradchao <brad@brad.tw>
2020-10-17 13:18:08 +0800

    14

--- app/src/main/java/tw/org/tcca/apps/test4/LoginActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test4/LoginActivity.java
@@ -1,11 +1,13 @@
 package tw.org.tcca.apps.test4;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -13,11 +15,15 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     private MainApp mainApp;
+    private EditText account, passwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mainApp = (MainApp)getApplication();
+        account = findViewById(R.id.login_account);
+        passwd = findViewById(R.id.login_passwd);
     }
 
     public void register(View view) {
@@ -40,6 +48,7 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         Log.v("bradlog", response);
+                        parseLogin(response);
                     }
                 },
                 new Response.ErrorListener() {
@@ -52,11 +61,37 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 HashMap<String,String> map = new HashMap<>();
+                map.put("account", account.getText().toString());
+                map.put("passwd", passwd.getText().toString());
                 return map;
             }
         };
         mainApp.queue.add(request);
     }
+
+    private void parseLogin(String json){
+        try {
+            JSONObject root = new JSONObject(json);
+            String login = root.getString("login");
+            if (login.equals("OK")){
+                gotoHome();
+            }else{
+                showErrorDialog();
+            }
+        } catch (JSONException e) {
+            Log.v("bradlog", e.toString());
+        }
+
+    }
+
+    private void showErrorDialog(){
+        new AlertDialog.Builder(this)
+                .setMessage("Login ERROR")
+                .setPositiveButton("OK", null)
+                .create()
+                .show();
+    }
+
     private void gotoHome(){
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
