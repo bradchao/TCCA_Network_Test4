31f488c66a03ecd247e7bd8dfd677232dd085136
bradchao <brad@brad.tw>
2020-10-17 09:43:36 +0800

    1

---
+++ app/src/main/java/tw/org/tcca/apps/test4/MainActivity.java
@@ -0,0 +1,19 @@
+package tw.org.tcca.apps.test4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    private RequestQueue queue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(this);
+    }
+}
